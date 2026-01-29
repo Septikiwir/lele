0f7c1ac21d78be88e7f837897bf706bbc3623b93
Septikiwir <wahyuseptapramudya@students.amikom.ac.id>
2026-01-29 23:08:02 +0700

    feat: Add NextAuth authentication setup, including API routes, middleware, and `trustHost` configuration.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,15 @@
 import { handlers } from "@/lib/auth"
+import { NextRequest } from "next/server"
 
 export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+export const revalidate = 0
 export const runtime = 'nodejs'
 
-export const { GET, POST } = handlers
+export async function GET(req: NextRequest) {
+    return handlers.GET(req)
+}
+
+export async function POST(req: NextRequest) {
+    return handlers.POST(req)
+}
